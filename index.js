36dff568aff7ccfb345fdd7ef1cc2a217cc08bbf
fariisiibrahiim <farisibrahmi@gmail.com>
2019-11-05 16:46:23 +0700

    First COmmit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let iteration = 0;
+while (iteration < 100) {
+  console.log("iteration" + iteration);
+  iteration += 5;
+}
+for (index = 0; index < 100; index += 3) {
+  console.log("index" + index);
+}
+const numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23];
+
+for (let index = 0; index < numbers.length; index++) {
+  const element = numbers[index];
+  console.log(element);
+}
+/*
+let input_number
+let input_multiplier = 
+while (input_multiplier)
+console.log(input_number*)
